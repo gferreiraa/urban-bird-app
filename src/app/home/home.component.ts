81fd63cd13409bcdd53bd40502fad72d4a8cec28
gferreira <gferreiraa@gmail.com>
2018-10-02 23:34:37 -0300

    Create service and model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OfertasServices } from './../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasServices ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor( private OfertasService: OfertasServices) { }
 
   ngOnInit() {
+    this.ofertas = this.OfertasService.getOfertas();
+    console.log(this.ofertas);
   }
 
 }
