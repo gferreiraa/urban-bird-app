727b116900d9068320ff4579fc19c33a567e7f04
gferreira <gferreiraa@gmail.com>
2018-10-12 00:06:59 -0300

    HTTP request component fun

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasServices } from '../ofertas.service';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [ OfertasServices ]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(private ofertasService: OfertasServices) { }
 
   ngOnInit() {
+    this.ofertasService.getOfertasPorCategoria('diversao')
+      .then(( ofertas: Oferta[] ) => {
+        this.ofertas = ofertas;
+      });
   }
 
 }
+
