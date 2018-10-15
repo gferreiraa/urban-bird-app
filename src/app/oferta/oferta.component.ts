f0220a0bc05ab6f8d395fc4c2c37f1936a0e8852
gferreira <gferreiraa@gmail.com>
2018-10-15 00:31:56 -0300

    Router params with snapshot

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-oferta',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // tslint:disable-next-line:no-unused-expression
+    this.route.snapshot.params['id'];
   }
 
 }
