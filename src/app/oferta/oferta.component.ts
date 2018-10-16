b61fc78baf16f3f0b561a1fe05c3cf77b1a4690f
gferreira <gferreiraa@gmail.com>
2018-10-15 22:34:38 -0300

    Created http request / API Rest

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,18 +1,28 @@
+import { Oferta } from './../shared/oferta.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasServices } from './../ofertas.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasServices ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor( private route: ActivatedRoute) { }
+  public oferta: Oferta;
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasServices
+    ) { }
 
   ngOnInit() {
-    // tslint:disable-next-line:no-unused-expression
-    this.route.snapshot.params['id'];
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+    .then((oferta: Oferta) => {
+      this.oferta = oferta;
+    });
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -18,4 +18,13 @@ export class OfertasServices {
             .toPromise()
             .then((resposta: any) => resposta.json());
     }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+      return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        .toPromise()
+        .then((resposta: any) => {
+          return resposta.json()[0];
+        });
+    }
+
 }
