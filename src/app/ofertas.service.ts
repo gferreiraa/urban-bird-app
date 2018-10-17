4d131ab295d58731e3a7d0729f2e505fb910fa34
gferreira <gferreiraa@gmail.com>
2018-10-17 00:09:00 -0300

    Adjustment url api access and created module api

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,28 +1,27 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasServices {
 
-    private url_api = 'http://localhost:3000/ofertas';
-
     constructor(private http: Http) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get(`${this.url_api}?destaque=true`)
+        return this.http.get(`${ URL_API }?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json());
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`${this.url_api}?categoria=${categoria}`)
+        return this.http.get(`${ URL_API }?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-      return this.http.get(`${this.url_api}?id=${id}`)
+      return this.http.get(`${ URL_API }?id=${id}`)
         .toPromise()
         .then((resposta: any) => {
           return resposta.json()[0];
