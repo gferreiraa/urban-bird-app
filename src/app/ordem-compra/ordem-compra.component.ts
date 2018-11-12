9979a77d0ca15e8dcf7a4fd025016015d704319a
gferreira <gferreiraa@gmail.com>
2018-11-12 00:45:55 -0200

    Apply Two-way-binding forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco = '';
+  public numero = '';
+  public complemento = '';
+  public formaPagamento = '';
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco;
+  }
+
+  public atualizaNumero (numero: string): void {
+    this.numero = numero;
+  }
+
+  public atualizarComplemente (complemento: string): void {
+    this.complemento = complemento;
+  }
+
+  public atualizaFormaPagamento (formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
   }
 
 }
