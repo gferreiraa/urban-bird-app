a34fdc496eab916727e8b4e4399e619a1c8aa3d3
gferreira <gferreiraa@gmail.com>
2018-11-12 23:47:37 -0200

    Apply visual feeback forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,11 @@ export class OrdemCompraComponent implements OnInit {
   public complemento = '';
   public formaPagamento = '';
 
+    // Controle de Validação dos Campos
+    public enderecoValido: boolean;
+    public numeroValido: boolean;
+    public complementoValido: boolean;
+    public formaPagamentoValido: boolean;
 
   constructor() { }
 
@@ -20,18 +25,40 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
+    // Regra para validar endereço: String maior que 3
+    if ( this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizaNumero (numero: string): void {
     this.numero = numero;
+    // Regra para validar número: Number maior ou igual a 2
+    if ( this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizarComplemente (complemento: string): void {
     this.complemento = complemento;
+    if ( this.complemento.length > 0) {
+      this.complementoValido = true;
+    }
   }
 
   public atualizaFormaPagamento (formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
+    if ( this.formaPagamento.length > 0 ) {
+      this.formaPagamentoValido = true;
+    } else{
+      this.formaPagamentoValido = false;
+    }
   }
+
+
 
 }
