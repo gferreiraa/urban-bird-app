c1076a5770c46b5d21eee7df1de19ad5fafdc965
gferreira <gferreiraa@gmail.com>
2018-11-16 00:01:33 -0200

    Implements http service

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,5 +1,11 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 import { Pedido } from './shared/pedido.model';
+@Injectable()
 export class OrdemCompraService {
+
+  constructor (private http: Http) {}
+
   public efetivarCompra(pedido: Pedido):  void {
     console.log(pedido);
   }
