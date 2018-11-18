95b975439d90697884f7a392c4a988313fe39f0d
gferreira <gferreiraa@gmail.com>
2018-11-17 23:45:07 -0200

    Create post request api

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,12 +1,23 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Pedido } from './shared/pedido.model';
+import { Observable } from 'rxjs';
+import { URL_API } from './app.api';
 @Injectable()
 export class OrdemCompraService {
 
   constructor (private http: Http) {}
 
-  public efetivarCompra(pedido: Pedido):  void {
-    console.log(pedido);
+  public efetivarCompra(pedido: Pedido): Observable<any> {
+
+    const headers: Headers = new Headers();
+
+    headers.append('Content-type', 'application/json');
+
+    return this.http.post(
+      `${URL_API}/pedidos`,
+       JSON.stringify(pedido),
+       new RequestOptions({ headers: headers })
+    );
   }
 }
