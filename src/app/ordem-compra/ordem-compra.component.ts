2b27718cc8c59e3068ff7625af3db0a2500a55c0
gferreira <gferreiraa@gmail.com>
2018-11-23 23:52:22 -0200

    Insert validator click

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,10 +12,10 @@ import { FormGroup, FormControlName, FormControl, Validators } from '@angular/fo
 export class OrdemCompraComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(4)]),
+    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3),     Validators.maxLength(4)]),
     'numero': new FormControl(null),
-    'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null)
+    'complemento': new FormControl(null, [Validators.required]),
+    'formaPagamento': new FormControl(null, [Validators.required])
   });
 
 
@@ -25,6 +25,13 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    console.log(this.formulario);
+    if (this.formulario.status === 'INVALID') {
+      console.log('Formulário Inávlido');
+      this.formulario.get('endereco').markAsTouched();
+      this.formulario.get('numero').markAsTouched();
+      this.formulario.get('complemento').markAsTouched();
+      this.formulario.get('formaPagamento').markAsTouched();
+
+    }
   }
 }
