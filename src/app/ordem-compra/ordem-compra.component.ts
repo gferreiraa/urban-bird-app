9b8d3f1806c32e0b93682a3d74c94f03fd82d84a
gferreira <gferreiraa@gmail.com>
2018-11-24 23:32:18 -0200

    HTTP post request / response

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,6 +11,8 @@ import { FormGroup, FormControlName, FormControl, Validators } from '@angular/fo
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: any;
+
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3),     Validators.maxLength(4)]),
     'numero': new FormControl(null),
@@ -26,12 +28,27 @@ export class OrdemCompraComponent implements OnInit {
 
   public confirmarCompra(): void {
     if (this.formulario.status === 'INVALID') {
-      console.log('Formulário Inávlido');
+      console.log('formulário está inválido');
+
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('complemento').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
 
+    } else {
+
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      );
+
+      this.ordemCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+          this.idPedidoCompra = idPedido;
+        });
     }
   }
+
 }
