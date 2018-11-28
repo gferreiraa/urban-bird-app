474a5619789d4d41f3b7a09f35e909cd1e069cc7
gferreira <gferreiraa@gmail.com>
2018-11-28 00:18:05 -0200

    Cart add amount

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -17,7 +17,15 @@ export class CarrinhoService {
       oferta.valor,
       1
     );
-    this.itens.push(itemCarrinho);
+    // Verifica a existência do item.
+    let itemCarrinhoEncontrado = this.itens.find( (item: ItemCarrinho) => item.id === itemCarrinho.id);
+
+    if ( itemCarrinhoEncontrado ) {
+      itemCarrinhoEncontrado.quantidade = itemCarrinhoEncontrado.quantidade + 1;
+    } else {
+      this.itens.push(itemCarrinho);
+    }
+
     console.log(itemCarrinho);
 
   }
